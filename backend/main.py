5a0f6d52509adc4c87554edd5e8f117dbb3b4f17
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-02-23 01:00:45 +0530

    Modified the main file just to test

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, requests, responses, HTTPException, status
 from src.core.config  import settings
 from src.db.session import engine
 from src.db.base import Base
-app = FastAPI()
+import uvicorn
 
 # class root:
 #     def home():
@@ -11,6 +11,7 @@ app = FastAPI()
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
+app = FastAPI()
 
 def start_application():
     app = FastAPI(title=settings.ProjectName, version=settings.ProjectVersion)
@@ -19,4 +20,4 @@ def start_application():
 
 @app.get("/")
 def home():
-    return {'data': 'You are on Home page'}
+    return {"data": "You are on dashboard/home page"}
