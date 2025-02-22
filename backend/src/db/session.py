1081da3d6d0727f5fa681c3070a622c4dea0c6d9
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-02-23 00:42:14 +0530

    Added the yield db and get_db function to assure the connection

--- backend/src/db/session.py
+++ backend/src/db/session.py
@@ -11,7 +11,16 @@ from core.config import settings
 
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-print("Database URL is ", SQLALCHEMY_DATABASE_URL)
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
+print(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+        print("Database connected")
+    finally:
+        print("Database Diconnected")
+        db.close()        
