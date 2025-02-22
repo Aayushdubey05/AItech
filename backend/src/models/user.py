4991cc9d63416e37a40f70be70311181f045eef1
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-02-23 00:51:12 +0530

    Modified the model properly for signup

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,1 +1,27 @@
-from src.core.config import startup_event, shutdown_event
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from sqlalchemy import Column, Integer, String
+from db.base import Base
+#defining the model for user signup
+
+# def create_tables():
+#     Base.metadata.create_all(bind=engine)
+
+# def start_application():
+#     app = FASTAPI(title=settings.ProjectName, version=settings.ProjectVersion)
+#     create_tables()
+#     return app
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    Name = Column(String)
+    Email = Column(String, unique=True, index=True)
+    Password = Column(String)
+    Mobile = Column(String)
+
+
+user = User()
