85b4efa3e891fb33c4fb01586d0577f17eb58f59
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-02-24 01:32:41 +0530

    Added the sigup router in and response generation on the api

--- backend/src/api/create.py
+++ backend/src/api/create.py
@@ -3,16 +3,16 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from fastapi import   APIRouter, HTTPException, Depends
-from schemas.user import UserCreation
+from schemas.user import UserCreation, UserResponse
 from service.signup import signup
 from sqlalchemy.orm import Session
 from db.session import get_db
 from models.user import User
 router = APIRouter()
 
-@router.post('/signup/', response_model=UserCreation )
+@router.post('/signup/', response_model=UserResponse )
 def newuser(createUser: UserCreation, db: Session = Depends(get_db)):
-    existing_user = db.query(User).filter(User.Email == createUser.Email).first()
+    existing_user = db.query(User).filter(User.email == createUser.Email).first()
     if existing_user:
         raise HTTPException(status_code=400, detail="User with that email id already exist")
     
