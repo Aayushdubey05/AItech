8b185382555307498413cac5b5093f22744c9450
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-03-17 02:25:02 +0530

    Enhanced the linkedin Scraper

--- backend/src/scraper/linkedinscraper.py
+++ backend/src/scraper/linkedinscraper.py
@@ -10,15 +10,59 @@ from concurrent.futures import ThreadPoolExecutor
 from playwright.sync_api import sync_playwright
 import playwright
 
-target_url_linkedin = "https://www.linkedin.com/jobs/"
+target_url_linkedin = "https://www.linkedin.com/"
 def run(url):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless= False)
+        browser = p.chromium.launch(headless= True)
         page = browser.new_page()
 
 
         page.goto(url)
+        print("Opened linkedin.com")
+
+        jobs_link = page.query_selector("a[href*='/jobs/search?trk=guest_homepage-basic_guest_nav_menu_jobs']")
+
+        if jobs_link:
+            jobs_link.click()
+            page.wait_for_load_state("domcontentloaded")
+            print("Navigated to the Jobs Page....")
+
+            #Detected the Sign-up pop to avoid that here is the code 
+            time.sleep(4)
+            try:
+                first = page.query_selector(".top-level-modal-container")
+                print("Accessed First")
+                second = first.query_selector(".modal")
+                print("Accessed Second")
+                third = second.query_selector(".modal__overlay")
+                print("accessed Third")
+                section = third.query_selector(".modal__wrapper")
+                print("accessed Forth")
+                close_btn = section.wait_for_selector(".modal__dismiss")
+
+                if close_btn:
+                    close_btn.click()
+                    print("Sign-In Popup Closed")
+            except Exception:
+                print("Signup pop up is not detected ")
+
+
+            #Waiting for job listing to be loaded 
+            page.wait_for_selector(".jobs-search__results-list")
+            print("Jobs Loaded...")
+
+            job_list = page.query_selector(".jobs-search__results-list")
+            jobs_item = job_list.query_selector_all("li")
+
+            print(f"Total number of jobs found: {len(jobs_item)}")
+
+            # for idx, job in enumerate(jobs_item, start=1):
+            #     job_id = job.get_attribute("data-job-id")
+            #     job_title = job.query_selector(".job-card-list__title").inner_text().strip()
+            #     print(f"{idx}. Job ID: {job_id} | Job title: {job_title}")
         
+        else:
+            print("Jobs link not found!")
 # def process_url(url):
 
 
