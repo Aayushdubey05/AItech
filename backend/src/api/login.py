0e838db878c9ee8b01b8e62e109f52198b0afcee
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-03-18 07:55:58 +0530

    Made it pretty good and added redirection

--- backend/src/api/login.py
+++ backend/src/api/login.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from db import session
 from fastapi import FastAPI, HTTPException, APIRouter, Depends, requests
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import Session
 from models.user import User
@@ -18,4 +19,8 @@ def logingin(Userexist: UserLogin, db:Session = Depends(get_db)):
     # Userexist.Email = requests.Request()
     # Userexist.Password = requests.Request()
     # loginfeature = Login(db, Userexist)
-    return Login(db, Userexist)
+    login_response = Login(db, Userexist)
+    if login_response.get("response model") == "Login Successfull":
+        return RedirectResponse(url=f"/profile/{Userexist.Email}", status_code=303)
+    else:
+        raise HTTPException(status_code=401, detail=login_response["response model"])
