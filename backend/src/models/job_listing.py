40729ebfdb4eb254ff401a22bd06e078dc86bf40
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-03-20 02:40:03 +0530

    Fix the bugs of ListedJobs

--- backend/src/models/job_listing.py
+++ backend/src/models/job_listing.py
@@ -8,14 +8,15 @@ from db.base import Base
 from db.session import SessionLocal, get_db
 from sqlalchemy import Column, String, INTEGER, VARCHAR, Integer
 class ListedJobs(Base):
-    id = Column(Integer, primary_key=True, index=True)
+    __tablename__='listed_jobs'
+    id = Column(Integer,primary_key=True,index=True,autoincrement=True)
     job_link = Column(String, unique=True, nullable=False)
     job_title = Column(String, nullable=False)
-    job_discription = Column(String, nullable=False)
-    job_type = Column(String, nullable=False)
+    job_description = Column(String, nullable=True)
+    job_type = Column(String, nullable=True)
     job_location = Column(String, nullable=False)
     job_company = Column(String, nullable=False)
-    
+
 
 
 
