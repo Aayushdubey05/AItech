22fe99132c2a8897833084b73b7c5179a9abdda3
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-03-23 00:50:27 +0530

    Made the schema to handle some data points

--- backend/src/schemas/jobsfetch.py
+++ backend/src/schemas/jobsfetch.py
@@ -5,14 +5,15 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from pydantic import BaseModel
 from models.job_listing import ListedJobs
+from typing import Optional
 
 
 class LinkedinJobs(BaseModel):
     JobTitle: str
     CompanyName: str
     JobLocation: str
-    JobDescription: str | None
-    JobType: str | None 
+    JobDescription: Optional[str] = None
+    JobType: Optional[str] = None
     JobLink: str
 
     class Config:
