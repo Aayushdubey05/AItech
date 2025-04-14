e7e616af26dc3b70bc3de1db155cdb66e36fae7c
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-04-14 16:45:08 +0530

    Updated the profile api

--- backend/src/api/profile.py
+++ backend/src/api/profile.py
@@ -2,7 +2,7 @@ import os
 import sys 
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from schemas.user import UserResponse, UserLogin
+from schemas.user import UserResponse, UserLogin, ProfileSchema
 from models.user import User
 from pydantic import EmailStr
 from fastapi import FastAPI, APIRouter, Depends, HTTPException
@@ -15,5 +15,19 @@ def getprofile(user_email_id: EmailStr, db:Session = Depends(get_db)):
     user = db.query(User).filter(User.email == user_email_id).first()
     if not user:
         raise HTTPException(status_code=404, detail={"error": "User not Found"})
-    
-    return{"We are still working on User Profile redirect to localhost:8000/jobs "}
+    ProfileResponse = ProfileSchema(Name=user.name,Email=user.email)
+    response = {
+        ProfileResponse.Name,
+        ProfileResponse.Email,
+        ProfileResponse.City,
+        ProfileResponse.States,
+        ProfileResponse.Country,
+        ProfileResponse.Resume,
+        ProfileResponse.ProfileImage,
+        ProfileResponse.LinkedinId,
+        ProfileResponse.GithubId,
+        ProfileResponse.CodingProfiles,
+        ProfileResponse.SkillSet,
+        ProfileResponse.DesiredRole
+    }
+    return{f"{response} \n We are still working on User Profile redirect to localhost:8000/jobs "}
