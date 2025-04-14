471963387ece89152701f24a39fd8c9b73ee06cf
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-04-14 16:48:17 +0530

    Deleted some files

--- backend/src/service/resume.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from schemas.resumeschema import ResumeDocuments
-from core.config import settings
-from db.session import get_db
-from models.user import Profile , User 
